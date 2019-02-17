0aa9b911d9088c8e7e2e7e164d70ae3b5078c4e8
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-02-17 19:13:35 +0000

    add mock request/response with jest and sinon

--- express-handlers.jest-test.js
+++ express-handlers.jest-test.js
@@ -1,1 +1,12 @@
+const mockRequest = (sessionData) => ({
+  session: { data: sessionData }
+});
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
 test.todo('should pass')
--- express-handlers.sinon-test.js
+++ express-handlers.sinon-test.js
@@ -1,3 +1,15 @@
-const test = require('ava')
+const test = require('ava');
+const sinon = require('sinon');
+
+const mockRequest = (sessionData) => ({
+  session: { data: sessionData }
+});
+
+const mockResponse = () => {
+  const res = {};
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.stub().returns(res);
+  return res;
+};
 
 test.todo('should succeed')
