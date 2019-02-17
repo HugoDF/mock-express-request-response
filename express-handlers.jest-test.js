ccaa22883b8adfe27435ac9626e6d0847436cdeb
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-02-17 19:08:58 +0000

    add login tests, add body to mockRequest

--- express-handlers.jest-test.js
+++ express-handlers.jest-test.js
@@ -1,5 +1,6 @@
-const mockRequest = (sessionData) => ({
-  session: { data: sessionData }
+const mockRequest = (sessionData, body) => ({
+  session: { data: sessionData },
+  body,
 });
 
 const mockResponse = () => {
@@ -9,9 +10,80 @@ const mockResponse = () => {
   return res;
 };
 
-const { logout, checkAuth } = require('./express-handlers');
+const { login, logout, checkAuth } = require('./express-handlers');
 
-// describe('login', () => {});
+describe('login', () => {
+  test('should 400 if username is missing from body', async () => {
+    const req = mockRequest(
+      {},
+      { password: 'boss' }
+    );
+    const res = mockResponse();
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'username and password are required'
+    });
+  });
+  test('should 400 if password is missing from body', async () => {
+    const req = mockRequest(
+      {},
+      { username: 'hugo' }
+    );
+    const res = mockResponse();
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'username and password are required'
+    });
+  });
+  test('should 401 with message if user with passed username does not exist', async () => {
+    const req = mockRequest(
+      {},
+      {
+        username: 'hugo-boss',
+        password: 'boss'
+      }
+    );
+    const res = mockResponse();
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'No user with matching username'
+    });
+  });
+  test('should 401 with message if passed password does not match stored password', async () => {
+    const req = mockRequest(
+      {},
+      {
+        username: 'guest',
+        password: 'not-good-password'
+      }
+    );
+    const res = mockResponse();
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Wrong password'
+    });
+  });
+  test('should 201 and set session.data with username if user exists and right password provided', async () => {
+    const req = mockRequest(
+      {},
+      {
+        username: 'guest',
+        password: 'guest-boss'
+      }
+    );
+    const res = mockResponse();
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalled();
+    expect(req.session.data).toEqual({
+      username: 'guest',
+    });
+  });
+});
 describe('logout', () => {
   test('should set session.data to null', async () => {
     const req = mockRequest({ username: 'hugo' });
--- express-handlers.sinon-test.js
+++ express-handlers.sinon-test.js
@@ -1,8 +1,9 @@
 const test = require('ava');
 const sinon = require('sinon');
 
-const mockRequest = (sessionData) => ({
-  session: { data: sessionData }
+const mockRequest = (sessionData, body) => ({
+  session: { data: sessionData },
+  body,
 });
 
 const mockResponse = () => {
@@ -12,7 +13,79 @@ const mockResponse = () => {
   return res;
 };
 
-const { logout, checkAuth } = require('./express-handlers');
+const { login, logout, checkAuth } = require('./express-handlers');
+
+test('login > should 400 if username is missing from body', async (t) => {
+  const req = mockRequest(
+    {},
+    { password: 'boss' }
+  );
+  const res = mockResponse();
+  await login(req, res);
+  t.true(res.status.calledWith(400));
+  t.true(res.json.calledWith({
+    message: 'username and password are required'
+  }));
+});
+test('should 400 if password is missing from body', async (t) => {
+  const req = mockRequest(
+    {},
+    { username: 'hugo' }
+  );
+  const res = mockResponse();
+  await login(req, res);
+  t.true(res.status.calledWith(400));
+  t.true(res.json.calledWith({
+    message: 'username and password are required'
+  }));
+});
+test('should 401 with message if user with passed username does not exist', async (t) => {
+  const req = mockRequest(
+    {},
+    {
+      username: 'hugo-boss',
+      password: 'boss'
+    }
+  );
+  const res = mockResponse();
+  await login(req, res);
+  t.true(res.status.calledWith(401));
+  t.true(res.json.calledWith({
+    message: 'No user with matching username'
+  }));
+});
+test('should 401 with message if passed password does not match stored password', async (t) => {
+  const req = mockRequest(
+    {},
+    {
+      username: 'guest',
+      password: 'not-good-password'
+    }
+  );
+  const res = mockResponse();
+  await login(req, res);
+  t.true(res.status.calledWith(401));
+  t.true(res.json.calledWith({
+    message: 'Wrong password'
+  }));
+});
+test('should 201 and set session.data with username if user exists and right password provided', async (t) => {
+  const req = mockRequest(
+    {},
+    {
+      username: 'guest',
+      password: 'guest-boss'
+    }
+  );
+  const res = mockResponse();
+  await login(req, res);
+  t.true(res.status.calledWith(201));
+  t.true(res.json.called);
+  t.deepEqual(
+    req.session.data,
+    { username: 'guest' }
+  );
+});
 
 test('logout > should set session.data to null', async (t) => {
   const req = mockRequest({ username: 'hugo' });
